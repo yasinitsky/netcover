a0026f9e6ef8895c52b3de1cef255abb44ba70aa
yasinitsky <yasinitskiy@gmail.com>
2022-04-05 20:04:38 +0200

    Empty entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-
-#include <netcover/buffers/Buffer.h>
-#include <netcover/sockets/raw/TCPv4Socket.h>
-
-using namespace Netcover;
-
-int main()
+int main(void)
 {
-    
-
     return 0;
 }
