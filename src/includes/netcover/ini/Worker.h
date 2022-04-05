3c84099d7dc53ae2d3ee7fa26a41a156711e3336
yasinitsky <yasinitskiy@gmail.com>
2022-04-05 18:21:25 +0200

    Ini Worker header file

---
+++ src/includes/netcover/ini/Worker.h
@@ -0,0 +1,23 @@
+#ifndef NETCOVER_INI_WORKER_H
+#define NETCOVER_INI_WORKER_H
+
+#include <string>
+#include <unordered_map>
+
+namespace Netcover::INI
+{
+    class Worker
+    {
+        public:
+            Worker(std::string file) : file(file) { }
+            void read();
+            void save();
+            std::unordered_map<std::string, std::unordered_map<std::string, std::string>>& getFileMap() { return fileMap; }
+
+        private:
+            std::unordered_map<std::string, std::unordered_map<std::string, std::string>> fileMap;
+            std::string file;
+    };
+}
+
+#endif
