a33b1777b7b8377c365294c0d4bb9d76191db22d
yasinitsky <yasinitskiy@gmail.com>
2022-05-04 00:04:28 +0200

    mutiple include protection

--- src/includes/netcover/tools/String.h
+++ src/includes/netcover/tools/String.h
@@ -1,6 +1,14 @@
+#ifndef NETCOVER_TOOLS_STRING_H
+#define NETCOVER_TOOLS_STRING_H
+
 #include <string>
+#include <algorithm>
+#include <cctype>
+#include <locale>
 
 namespace Netcover::Tools::String
 {
     void trim(std::string& str);
 }
+
+#endif
