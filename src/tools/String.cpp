a9bcdee1d3ea8ed06dc7cbd6ad30ec63677b20c2
yasinitsky <yasinitskiy@gmail.com>
2022-05-04 02:19:18 +0200

    pop_front() definition

--- src/tools/String.cpp
+++ src/tools/String.cpp
@@ -1,7 +1,5 @@
 #include <netcover/tools/String.h>
 
-using namespace Netcover::Tools::String;
-
 static inline void ltrim(std::string& str)
 {
     str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char ch){
@@ -13,11 +11,16 @@ static inline void rtrim(std::string& str)
 {
     str.erase(std::find_if(str.rbegin(), str.rend(), [](unsigned char ch){
         return !std::isspace(ch);
-    }).base(), s.end());
+    }).base(), str.end());
 }
 
-inline void trim(std::string& str)
+void Netcover::Tools::String::trim(std::string& str)
 {
     ltrim(str);
     rtrim(str);
+}
+
+void Netcover::Tools::String::pop_front(std::string& str)
+{
+    str.erase(0, 1);
 }
