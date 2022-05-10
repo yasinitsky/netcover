f12fca0bb2f13ade1dbd47f09665bbaaaaeda504
yasinitsky <yasinitskiy@gmail.com>
2022-05-10 18:07:14 +0200

    setFlagList(std::pair<std::string, std::string>) decalaration

--- src/includes/netcover/cli/Arguments.h
+++ src/includes/netcover/cli/Arguments.h
@@ -10,6 +10,7 @@ namespace Netcover::CLI
             Arguments(int argc, char* argv[]);
             void parseFlags();
             std::vector<std::string> operator*() { return flags; }
+            void setFlagList(std::pair<std::string, std::string>);
         private:
             std::vector<std::string> args;
             std::vector<std::string> flags;
