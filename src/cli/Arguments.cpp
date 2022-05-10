85d64e7348ca9f28ae6548a7ab36b8de4f747dd4
yasinitsky <yasinitskiy@gmail.com>
2022-05-10 20:05:09 +0200

    fix error CLI::Arguments::Arguments()

--- src/cli/Arguments.cpp
+++ src/cli/Arguments.cpp
@@ -1,10 +1,11 @@
 #include <netcover/cli/Arguments.h>
+#include <netcover/tools/String.h>
 
 using namespace Netcover::CLI;
 
 Arguments::Arguments(int argc, char* argv[])
 {
-    for(int i = 0; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
         args.push_back(std::string(argv[i]));
     }
